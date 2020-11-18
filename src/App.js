00a5a5fb785bea16be56e98b17d713ccb243a9a7
Muhammad Osama Shahzad <usamashahzad19@gmail.com>
2020-11-18 09:31:06 +0500

    added value context file and modified App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import React, {useState} from 'react';
 import './App.css';
 import Parent from './Parent';
+import valueContext from './valueContext';
 
 function App() {
   let [number, setNumber] = useState(50);
+  let value = 81;
   return (
+    <ValueContext.Provider value = {value}>
     <div>
      Hello World
      <Parent num = {number}> </Parent>
      <button onClick = {() => {setNumber(++number)}}> Update Number</button>
     </div>
+    </ValueContext.Provider>
   );
 }
 
