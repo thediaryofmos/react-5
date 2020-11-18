eabfb968de96f82d650ed793a7889ed450e486af
Muhammad Osama Shahzad <usamashahzad19@gmail.com>
2020-11-18 10:11:18 +0500

    Added global variable in valueContext and passed it from App.js to Parent to Child.js

--- src/Child.js
+++ src/Child.js
@@ -1,7 +1,11 @@
+import valueContext from './valueContext';
+import React, {useContext} from 'react';
+
 function Child(props) {
+  let value = useContext(ValueContext);
     return (
       <div className="App">
-       Child
+       Child Number: {value}
        Number: {props.num}
       </div>
     );
